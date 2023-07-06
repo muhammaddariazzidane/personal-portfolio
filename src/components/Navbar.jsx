ba3a80e6c551d62d1238e2b61b6cb7844031b7e4
muhammaddariazzidane <dariazzidane@gmail.com>
2023-07-06 07:49:20 +0700

    fix toggle navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
   const [smallMenu, setSmallMenu] = useState(false);
@@ -37,9 +38,7 @@ export default function Navbar() {
             </li>
           </ul>
           <button className="flex justify-end  items-center lg:hidden relative" onClick={handleSmallMenu}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
+            {smallMenu ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
             <div className={`absolute top-10 shadow-md rounded-tl-lg rounded-bl-lg rounded-br-lg w-36 bg-white ${smallMenu ? "scale-100 transition-all duration-500" : "scale-0 origin-top-right rotate-90 transition-all duration-500"}`}>
               <ul className="flex flex-col space-y-2 items-center p-3">
                 <li className="hover:text-white transition-all duration-300 rounded-md hover:shadow hover:bg-indigo-500 px-4 py-1">
